a1e087586633d77a333dbedf2e33f45920dd7158
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-08-13 17:41:30 +0200

    Hero file complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import { useState } from "react";
 import Background from "./components/Background/Background.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-
-
+import Hero from "./components/Hero/Hero.jsx";
 
 function App() {
   let heroData = [
-    {text1: "Dive into",text2:"what you love"},
-    {text1: "Induldge",text2:"your passions"},
-    {text1: "Give in to",text2:"your passions"},
-  ]
-  const [heroCount,setHerocount] = useState(0);
-  const [playStatus,setPlayStatus] = useState(false);
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Induldge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" },
+  ];
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
   return (
     <div>
-      <Background 
-      playStatus={playStatus} heroCount={heroCount}
-      />
+      <Background playStatus={playStatus} heroCount={heroCount} />
       <Navbar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
-
-
-
