2a6a1565ceff22cf6242e8c1dedd42adf4022d5b
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-08-13 16:43:25 +0200

    background and navbar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useState } from "react";
+import Background from "./components/Background/Background.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+
+
 
 function App() {
+  let heroData = [
+    {text1: "Dive into",text2:"what you love"},
+    {text1: "Induldge",text2:"your passions"},
+    {text1: "Give in to",text2:"your passions"},
+  ]
+  const [heroCount,setHerocount] = useState(0);
+  const [playStatus,setPlayStatus] = useState(false);
   return (
     <div>
-      
+      <Background 
+      playStatus={playStatus} heroCount={heroCount}
+      />
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
-import "./navbar.css"
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="nav">
+        <div className="nav-logo">My-React</div>
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>Explore</li>
+            <li>About</li>
+            <li className="nav-contact">Contact</li>
+        </ul>
       
     </div>
   )
